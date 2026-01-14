67965a3c85af49c902a93e48f0c86bf7c8555d8d
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2026-01-14 10:30:21 -0300

    feat: adding new consumers and producers

--- produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,19 +13,28 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
 
+        // mensagem para novo pedido
         String value = "123123,pedido1,309183091301";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
 
-        // o método send retorna um Future, não nos deixando saber se a msg foi enviada com sucesos
-        // por isso estamos passando um callback para ele
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("[SUCCESS] TOPIC::" + data.topic() + ":::PARTITION->" + data.partition() + "/OFFSET->" + data.offset() + "/TIMESTAMP->" + data.timestamp());
-        }).get();
+        };
 
+        // o método send retorna um Future, não nos deixando saber se a msg foi enviada com sucesos
+        // por isso estamos passando um callback para ele
+        producer.send(record, callback).get();
+
+        // mensagem para enviar email
+        var title = "Thanks for your order";
+        var body = "We're processing your products.";
+        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", title, body);
+
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
