f1c89d2c05e1d884671e328244cc9a70c3906e54
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2026-01-14 19:27:56 -0300

    feat: adding parallel consumers, using partition keys, and setting max poll size

--- produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 public class FraudDetectorService {
     public static void main(String[] args) {
@@ -15,7 +16,9 @@ public class FraudDetectorService {
         consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
 
         while(true) {
-            var records = consumer.poll(Duration.ofMillis(100));
+            var records = consumer.poll(Duration.ofMillis(100)); // esses 100ms são uma espécie de timeout
+                                                                 // que esperaremos até que existam mensagens no tópico
+                                                                 // até seguir a execução do código.
 
             if (!records.isEmpty()) {
                 System.out.println(records.count() + " record(s) found.");
@@ -50,6 +53,19 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         // a declaração do grupo é fundamental para iniciar o consumidor
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+
+        // podemos passar um id para o consumidor
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, String.format("%s-%s", FraudDetectorService.class.getSimpleName(), UUID.randomUUID().toString()));
+
+        // a config max.poll.records diz para o kafka quantas mensagens enviar a cada poll
+
+        // ela tem relação com o tempo de processamento (max.poll.interval.ms)
+        // que é o tempo que devemos terminar de processar as mensagens buscadas em cada poll.
+        //
+        // caso contrário, o kafka entenderá que o consumidor "travou",
+        // expulsará ele do grupo e iniciará um rebalanceamento.
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+
 
         return properties;
     }
--- produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -7,34 +7,37 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
 
-        // mensagem para novo pedido
-        String value = "123123,pedido1,309183091301";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        for(var i = 0; i < 100; i++) {// mensagem para novo pedido
+            String newOrderKey = UUID.randomUUID().toString(); // a key é fundamental para garantir a distribuição
+            // das mensagens entre os tópicos existentes
+            String value = "123123,pedido1,309183091301";
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", newOrderKey, value);
 
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("[SUCCESS] TOPIC::" + data.topic() + ":::PARTITION->" + data.partition() + "/OFFSET->" + data.offset() + "/TIMESTAMP->" + data.timestamp());
-        };
+            Callback callback = (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println("[SUCCESS] TOPIC::" + data.topic() + ":::PARTITION->" + data.partition() + "/OFFSET->" + data.offset() + "/TIMESTAMP->" + data.timestamp());
+            };
 
-        // o método send retorna um Future, não nos deixando saber se a msg foi enviada com sucesos
-        // por isso estamos passando um callback para ele
-        producer.send(record, callback).get();
+            // o método send retorna um Future, não nos deixando saber se a msg foi enviada com sucesos
+            // por isso estamos passando um callback para ele
+            producer.send(record, callback).get();
 
-        // mensagem para enviar email
-        var title = "Thanks for your order";
-        var body = "We're processing your products.";
-        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", title, body);
+            // mensagem para enviar email
+            var body = "Thanks for your order! We're processing your products.";
+            var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", newOrderKey, body);
 
-        producer.send(emailRecord, callback).get();
+            producer.send(emailRecord, callback).get();
+        }
     }
 
     private static Properties properties() {
