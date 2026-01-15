e9ba6c5c291998a75608b0e2651ff85d46cbef72
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2026-01-15 17:48:50 -0300

    feat: creating generic KafkaService and KafkaDispatcher

--- produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,72 +1,35 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class FraudDetectorService {
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
+        var fraudService = new FraudDetectorService();
+        try(var service = new KafkaService(
+                FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse)) {
 
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-
-        while(true) {
-            var records = consumer.poll(Duration.ofMillis(100)); // esses 100ms são uma espécie de timeout
-                                                                 // que esperaremos até que existam mensagens no tópico
-                                                                 // até seguir a execução do código.
-
-            if (!records.isEmpty()) {
-                System.out.println(records.count() + " record(s) found.");
-
-                for (var record : records) {
-                    System.out.println("------------------------------------------------");
-                    System.out.println("Processing new order, checking for fraud...");
-                    System.out.println("- Key::" + record.key());
-                    System.out.println("- Value::" + record.value());
-                    System.out.println("- Partition::" + record.partition());
-                    System.out.println("- Offset::" + record.offset());
-                    System.out.println("- Topic::" + record.topic());
-
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        // ignoring
-                        e.printStackTrace();
-                    }
-
-                    System.out.println("Order processed!");
-                }
-            }
+            service.run();
         }
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------------");
+        System.out.println("Processing new order, checking for fraud...");
+        System.out.println("- Key::" + record.key());
+        System.out.println("- Value::" + record.value());
+        System.out.println("- Partition::" + record.partition());
+        System.out.println("- Offset::" + record.offset());
+        System.out.println("- Topic::" + record.topic());
 
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        // a declaração do grupo é fundamental para iniciar o consumidor
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
 
-        // podemos passar um id para o consumidor
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, String.format("%s-%s", FraudDetectorService.class.getSimpleName(), UUID.randomUUID().toString()));
-
-        // a config max.poll.records diz para o kafka quantas mensagens enviar a cada poll
-
-        // ela tem relação com o tempo de processamento (max.poll.interval.ms)
-        // que é o tempo que devemos terminar de processar as mensagens buscadas em cada poll.
-        //
-        // caso contrário, o kafka entenderá que o consumidor "travou",
-        // expulsará ele do grupo e iniciará um rebalanceamento.
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
-
-
-        return properties;
+        System.out.println("Order processed!");
     }
 }
---
+++ produtores-e-consumidores-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -0,0 +1,58 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.Closeable;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+public class KafkaService implements Closeable {
+
+    private final KafkaConsumer<String, String> consumer;
+    private final ConsumerFunction parse;
+
+    public KafkaService(String groupName, String topic, ConsumerFunction parse) {
+        this.consumer = new KafkaConsumer<>(properties(groupName));
+        this.parse = parse;
+
+        consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    public void run() {
+        while(true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+
+            if (!records.isEmpty()) {
+                System.out.println(records.count() + " record(s) found.");
+
+                for (var record : records) {
+                    parse.consume(record);
+                }
+            }
+        }
+    }
+
+    private static Properties properties(String groupName) {
+        var properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // a declaração do grupo é fundamental para iniciar o consumidor
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupName);
+
+        // podemos passar um id para o consumidor
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, String.format("%s-%s", groupName, UUID.randomUUID().toString()));
+
+        return properties;
+    }
+
+    @Override
+    public void close() {
+        this.consumer.close();
+    }
+}
