b31ecf66ae41430364768a9b1c453c647431838d
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2026-01-16 17:18:59 -0300

    refactor: updating Order model to include email attribute

--- fast-delegate-evolucao-e-cluster-de-brokers/ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ fast-delegate-evolucao-e-cluster-de-brokers/ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -12,12 +12,13 @@ public class NewOrderMain {
                     var userId = UUID.randomUUID().toString();
                     var orderId = UUID.randomUUID().toString();
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var email = Math.random() + "@email.com";
 
-                    var order = new Order(userId, orderId, amount);
+                    var order = new Order(userId, orderId, amount, email);
                     orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                    var email = new Email("Thanks for your order!", "We're processing your products.");
-                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                    var successEmail = new Email("Thanks for your order!", "We're processing your products.");
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, successEmail);
                 }
             }
         }
