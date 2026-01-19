d19558aa97b442b6e6103476fd57b88953a8214c
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2026-01-19 19:40:27 -0300

    feat: fast delegating the task to create reading reports

---
+++ batches-correlation-ids-e-dead-letters/ecommerce/service-http-ecommerce/src/main/java/br/com/alura/ecommerce/GenerateAllReportsServlet.java
@@ -0,0 +1,45 @@
+package br.com.alura.ecommerce;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class GenerateAllReportsServlet extends HttpServlet {
+    private final KafkaDispatcher<String> taskDispatcher = new KafkaDispatcher<>();
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        this.taskDispatcher.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+
+            // aqui estamos fazendo um fast delegate, mandando uma mensagem "cedo",
+            // retornando rápido para o usuário e fazendo o resto do processamento
+            // pesado em "background". Isso é útil pois evita que qualquer problema
+            // no servidor cause perda de dados, por exemplo.
+            taskDispatcher.send(
+                    "SEND_MESSAGE_TO_ALL_USERS",
+                    UUID.randomUUID().toString(), // usando um randomUUID como key para ter paralelismo com consumidores em partições diferentes
+                    "USER_GENERATE_READING_REPORT"
+            );
+
+            System.out.println("Sent generate report to all users");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println("Report requests generated!");
+
+        } catch (ExecutionException e) {
+            throw new ServletException(e);
+        } catch (InterruptedException e) {
+            throw new ServletException(e);
+        }
+    }
+}
