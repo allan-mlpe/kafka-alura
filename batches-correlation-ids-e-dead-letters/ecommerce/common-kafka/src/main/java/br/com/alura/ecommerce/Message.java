15c1d831085bc8cb02c1e3ae9fc64d2b4f8a1239
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2026-01-20 16:55:01 -0300

    feat: creating generic Message to wrap sent messages and add to it a correlation id

---
+++ batches-correlation-ids-e-dead-letters/ecommerce/common-kafka/src/main/java/br/com/alura/ecommerce/Message.java
@@ -0,0 +1,19 @@
+package br.com.alura.ecommerce;
+
+public class Message<T> {
+    private final CorrelationId correlationId;
+    private final T payload;
+
+    Message(CorrelationId correlationId, T payload) {
+        this.correlationId = correlationId;
+        this.payload = payload;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "correlationId=" + correlationId +
+                ", payload=" + payload +
+                '}';
+    }
+}
