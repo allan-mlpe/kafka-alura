654bb6cd4cff9768921e4488a457ac96cac6ce8f
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2026-01-22 21:43:10 -0300

    feat: handling idempotency to process messages only once
    commit aa277f667acadf9c9380b71be028c283623086d0
    Author: Allan Monteiro de Lima <allan_mlpe@hotmail.com>
    Date:   Thu Jan 22 21:41:45 2026 -0300
    
        refactor: updating NewOrderServlet to use a database to control idempotency
    
    commit a7059b72d326483c62502fcd4a645465c375216d
    Author: Allan Monteiro de Lima <allan_mlpe@hotmail.com>
    Date:   Thu Jan 22 21:41:14 2026 -0300
    
        chore: adding service databases to gitignore
    
    commit b0638fb329b83be261b30b517dc80d89cdeed091
    Author: Allan Monteiro de Lima <allan_mlpe@hotmail.com>
    Date:   Thu Jan 22 21:40:43 2026 -0300
    
        refactor: updating update method to return a boolean
    
    commit 798273712663c6eed7f70fac178fe3cf3c7644f0
    Author: Allan Monteiro de Lima <allan_mlpe@hotmail.com>
    Date:   Thu Jan 22 21:08:35 2026 -0300
    
        feat: making FraudDetectorService use a database to store processed orders and its fraud detection result
    
    commit 8d0153febf895e159cc96505ced68e448401ed27
    Author: Allan Monteiro de Lima <allan_mlpe@hotmail.com>
    Date:   Thu Jan 22 20:48:27 2026 -0300
    
        refactor: updating CreateUserService to use common-database
    
    commit 2f016d93ed83b440300182961e6ccb0726479cf2
    Author: Allan Monteiro de Lima <allan_mlpe@hotmail.com>
    Date:   Thu Jan 22 20:48:04 2026 -0300
    
        feat: creating a common-database module to abstract things related to databases
    
    commit 407b345821f6ee2f4df32ccbae38c7e7a7cf154f
    Author: Allan Monteiro de Lima <allan_mlpe@hotmail.com>
    Date:   Thu Jan 22 20:06:50 2026 -0300
    
        chore: removing unused exceptions
    
    commit b9e9baa46b4adc59587e0f935187fe968ed729bb
    Author: Allan Monteiro de Lima <allan_mlpe@hotmail.com>
    Date:   Thu Jan 22 20:03:05 2026 -0300
    
        refactor: updating CreateUserService to implement ConsumerService

--- idempotência-e-garantias/ecommerce/service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
+++ idempotência-e-garantias/ecommerce/service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.UUID;
+import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderServlet extends HttpServlet {
@@ -27,25 +27,38 @@ public class NewOrderServlet extends HttpServlet {
         try {
             // we're not caring about any security issues, we're only
             // showing how to use http as a starting point to send messages
-            var orderId = UUID.randomUUID().toString();
+            var orderId = req.getParameter("uuid");
             var amount = new BigDecimal(req.getParameter("amount"));
             var email = req.getParameter("email");
 
             var order = new Order(orderId, amount, email);
-            this.orderDispatcher.send(
-                    "ECOMMERCE_NEW_ORDER",
-                    email,
-                    new CorrelationId(NewOrderServlet.class.getSimpleName()),
-                    order);
 
-            System.out.println("New order sent successfully");
+            // estamos criando uma conexão com o banco a cada request para fins didáticos
+            // no mundo real, poderíamos usar, por exemplo, um pool de conexões.
+            try(var database = new OrdersDatabase()) {
+                if (database.saveNew(order)) {
+                    this.orderDispatcher.send(
+                            "ECOMMERCE_NEW_ORDER",
+                            email,
+                            new CorrelationId(NewOrderServlet.class.getSimpleName()),
+                            order);
 
-            resp.setStatus(HttpServletResponse.SC_OK);
-            resp.getWriter().println("New order sent!");
+                    System.out.println("New order sent successfully");
+
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.getWriter().println("New order sent!");
+
+                } else {
+                    System.out.println("Old order received.");
+
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.getWriter().println("Old order received!");
+                }
+            }
 
         } catch (ExecutionException e) {
             throw new ServletException(e);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | SQLException e) {
             throw new ServletException(e);
         }
     }
--- idempotência-e-garantias/ecommerce/service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
+++ idempotência-e-garantias/ecommerce/service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
@@ -1,47 +1,29 @@
 package br.com.alura.ecommerce;
 
-import br.com.alura.ecommerce.consumer.KafkaService;
+import br.com.alura.ecommerce.consumer.ConsumerService;
+import br.com.alura.ecommerce.consumer.ServiceRunner;
+import br.com.alura.ecommerce.database.LocalDatabase;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.sql.*;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
-public class CreateUserService {
+public class CreateUserService implements ConsumerService<Order> {
 
-    private final Connection connection;
+    private final LocalDatabase database;
 
     CreateUserService() throws SQLException {
-        String url = "jdbc:sqlite:users_database.db";
-        this.connection = DriverManager.getConnection(url);
-
-        // cria a tabela
-        try {
-            connection.createStatement().execute("create table Users (" +
-                    "uuid varchar(200) primary key," +
-                    "email varchar(200))");
-        } catch (SQLException e) {
-            // be careful, the sql could be wrong.
-            // we're doing like this for "educational purposes" to focus on Kafka
-            e.printStackTrace();
-        }
-
+        this.database = new LocalDatabase("users_database");
+        this.database.createIfNotExists("create table Users (" +
+                "uuid varchar(200) primary key," +
+                "email varchar(200))");
     }
 
-    public static void main(String[] args) throws SQLException, ExecutionException, InterruptedException {
-        var createUserService = new CreateUserService();
-        try(var service = new KafkaService<>(
-                CreateUserService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                createUserService::parse,
-                Map.of())) {
-
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner<>(CreateUserService::new).start(1);
     }
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws SQLException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws SQLException {
         System.out.println("------------------------------------------------");
         System.out.println("Processing new order, checking for new user...");
         System.out.println("- Value::" + record.value().toString());
@@ -53,26 +35,27 @@ public class CreateUserService {
         if (isNewUser(email)) {
             insertUser(email);
         }
+    }
 
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return CreateUserService.class.getSimpleName();
     }
 
     private void insertUser(String email) throws SQLException {
-        PreparedStatement insert =
-                connection.prepareStatement("insert into Users (uuid, email) values (?, ?)");
         var userId = UUID.randomUUID().toString();
-
-        insert.setString(1, userId);
-        insert.setString(2, email);
-        insert.execute();
+        database.update("insert into Users (uuid, email) values (?, ?)", userId, email);
 
         System.out.println(String.format("Usuário %s com email %s adicionado", userId, email));
     }
 
     private boolean isNewUser(String email) throws SQLException {
-        var query = connection.prepareStatement("select uuid from Users where email = ? limit 1");
-        query.setString(1, email);
-
-        ResultSet results = query.executeQuery();
+        ResultSet results = database.query("select uuid from Users where email = ? limit 1", email);
 
         return !results.next();
     }
